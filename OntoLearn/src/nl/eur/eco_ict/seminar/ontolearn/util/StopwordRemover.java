fdb5b08841ef692bd4c2a82b0bba256e44815e79
jasper <jasper@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-15 16:22:45 +0000

    - Some instructions on how to deal with build path problems - Some startingpoints for Hesing: interface first implementation later

---
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/util/StopwordRemover.java
@@ -0,0 +1,17 @@
+/**
+ * OntoLearn a seminar project of:
+ * - Remy Stibbe
+ * - Hesing Kuo
+ * - Nico Vaatstra
+ * - Jasper Voskuilen
+ * 
+ */
+package nl.eur.eco_ict.seminar.ontolearn.util;
+
+/**
+ * @author Jasper
+ *
+ */
+public interface StopwordRemover {
+
+}
