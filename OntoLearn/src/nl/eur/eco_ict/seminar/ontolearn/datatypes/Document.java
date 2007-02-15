9a1c4a154d98c1721183a71d2452b6efb6d052c7
jasper <jasper@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-15 04:28:47 +0000

    Implementation based on Nico's code with some optimization's

--- OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/datatypes/Document.java
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/datatypes/Document.java
@@ -7,12 +7,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
 
 /**
  * @author 300353jv
  */
 public class Document {
 	private File file = null;
+	private StringBuffer abstrct = null;
 
 	public Document (File f) {
 		this.file = f;
@@ -23,8 +26,62 @@ public class Document {
 	 * @throws FileNotFoundException
 	 */
 	public BufferedReader read () throws FileNotFoundException {
+		if (this.abstrct == null && this.isRedifDocument ()){
+			this.extractAbstracts ();
+		}else{
+			this.abstrct = new StringBuffer();
+		}
+		return new BufferedReader (new StringReader (this.abstrct.toString ()
+				));
+	}
+	
+	/**
+	 * @return it's a redif document when it starts with 'Template-Type: ReDIF-Paper'
+	 * @throws FileNotFoundException 
+	 */
+	protected boolean isRedifDocument () throws FileNotFoundException{
+		final String header = "Template-Type: ReDIF-Paper";
+		if (this.file != null && this.file.canRead ()){
+			BufferedReader br = new BufferedReader (new FileReader(this.file));
+			String line = null;
+			try {
+				if ((line=br.readLine ())!=null){
+					return line.contains (header);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Assumes it's a REDIF file and searches the file for abstracts. When an abstract is found it is put into the global StringBuffer (a cache for repeated requests)
+	 * @throws FileNotFoundException
+	 */
+	protected void extractAbstracts () throws FileNotFoundException{
+		final String abstractHeader = "Abstract:";
+		final String regEndAbstract = "^([a-zA-Z\\x2D]+)(\\x3A{1})(.)*$";
+		this.abstrct = new StringBuffer();
+		BufferedReader br = new BufferedReader(new FileReader(this.file));
+		String line = null;
+		boolean extract = false;
 		
-		// Hier kan eventueel wat extra code waarmee de abstract eruit gefilterd wordt
-		return new BufferedReader (new FileReader (this.file));
+		try{
+		while((line = br.readLine ()) != null){
+			if (line.matches (regEndAbstract)){
+				extract = false;
+			}
+			if (extract){
+				this.abstrct.append (line);
+			}
+			if (line.startsWith (abstractHeader)){
+				extract = true; // take the next lines
+				this.abstrct.append (line.substring (10, line.length ()));
+			}
+		}
+		}catch(Exception e){
+			e.printStackTrace ();
+		}
 	}
 }
