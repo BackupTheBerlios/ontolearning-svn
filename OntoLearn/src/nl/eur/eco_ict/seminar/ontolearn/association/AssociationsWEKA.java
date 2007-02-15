18ee269193428303306912e2eb303b7c33aa1dfa
stibbe <stibbe@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-15 17:51:51 +0000

    1e opzet

---
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/association/AssociationsWEKA.java
@@ -0,0 +1,17 @@
+/**
+ * OntoLearn a seminar project of:
+ * - Remy Stibbe
+ * - Hesing Kuo
+ * - Nico Vaatstra
+ * - Jasper Voskuilen
+ * 
+ */
+package nl.eur.eco_ict.seminar.ontolearn.association;
+
+/**
+ * @author remy
+ *
+ */
+public class AssociationsWEKA {
+
+}
