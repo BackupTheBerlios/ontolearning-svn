d3ee1141d48accf38a9aeac70be9636bc4bd0322
stibbe <stibbe@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-18 12:48:35 +0000

    parse function added

--- OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/extractor/AssociationBasedExtractor.java
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/extractor/AssociationBasedExtractor.java
@@ -6,10 +6,15 @@ package nl.eur.eco_ict.seminar.ontolearn.extractor;
 import nl.eur.eco_ict.seminar.ontolearn.Extractor;
 import nl.eur.eco_ict.seminar.ontolearn.Ontology;
 import nl.eur.eco_ict.seminar.ontolearn.datatypes.Document;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+import java.io.FileNotFoundException;
+import java.util.List;
 
 /**
  * @author 300353jv
- *
+ * @author Remy
  */
 public class AssociationBasedExtractor implements Extractor {
 
@@ -17,8 +22,18 @@ public class AssociationBasedExtractor implements Extractor {
 	 * @see nl.eur.eco_ict.seminar.ontolearn.Extractor#parse(nl.eur.eco_ict.seminar.ontolearn.datatypes.Document, nl.eur.eco_ict.seminar.ontolearn.Ontology)
 	 */
 	public void parse (Document doc, Ontology ontology) {
-		// TODO Auto-generated method stub
-		
-	}
+		System.out.println("Association Rules are parsing.");
+		try {
+			MaxentTagger posTagger = new MaxentTagger();
+			List myList = posTagger.tokenizeText(doc.readAbstracts());
 
+			for (int x = 0, mySize = myList.size(); x < mySize; x++) {
+				Sentence mySentence = (Sentence) myList.get(x);
+				String myPOSString = posTagger.tagSentence(mySentence).toString();
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Error: "+e);
+		}
+	}
 }
