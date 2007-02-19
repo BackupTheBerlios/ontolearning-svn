3adb0e241f46badd200a6e5426e087e5ace27025
stibbe <stibbe@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-19 16:24:06 +0000

    Eerste versie van de Occurance Class. Dit is het eerste begin voor output naar WEKA

---
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/association/Occurance.java
@@ -0,0 +1,19 @@
+/**
+ * OntoLearn a seminar project of:
+ * - Remy Stibbe
+ * - Hesing Kuo
+ * - Nico Vaatstra
+ * - Jasper Voskuilen
+ * 
+ */
+package nl.eur.eco_ict.seminar.ontolearn.association;
+
+/**
+ * @author remy
+ *
+ */
+public class Occurance {
+	public String documentName;
+	public String word;
+	public int wordCount;
+}
