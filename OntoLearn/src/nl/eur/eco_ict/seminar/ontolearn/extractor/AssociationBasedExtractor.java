6635844269417d3a7c11870771660536573fd8f3
stibbe <stibbe@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-19 13:28:13 +0000

    Jasper heeft de diepe materie van het ingewikkelde javaproggen opgelost en een fantasiche oplossing voor geboden.

--- OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/extractor/AssociationBasedExtractor.java
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/extractor/AssociationBasedExtractor.java
@@ -6,6 +6,8 @@ package nl.eur.eco_ict.seminar.ontolearn.extractor;
 import nl.eur.eco_ict.seminar.ontolearn.Extractor;
 import nl.eur.eco_ict.seminar.ontolearn.datatypes.Document;
 import nl.eur.eco_ict.seminar.ontolearn.datatypes.Ontology;
+import nl.eur.eco_ict.seminar.ontolearn.util.PartOfSpeechTagger;
+import nl.eur.eco_ict.seminar.ontolearn.util.Tokenizer;
 import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
@@ -24,16 +26,26 @@ public class AssociationBasedExtractor implements Extractor {
 	public void parse (Document doc, Ontology ontology) {
 		System.out.println("Association Rules are parsing "+doc.getName ()+".");
 		try {
-			MaxentTagger posTagger = new MaxentTagger();
-			List myList = posTagger.tokenizeText(doc.readAbstracts());
+			PartOfSpeechTagger posTagger = PartOfSpeechTagger.Factory.getInstance();
+			Tokenizer tokenizer = Tokenizer.Factory.getInstance();
+			List<String> myList = tokenizer.toSentences (doc.readAbstracts());
 
 			for (int x = 0, mySize = myList.size(); x < mySize; x++) {
-				Sentence mySentence = (Sentence) myList.get(x);
-				String myPOSString = posTagger.tagSentence(mySentence).toString();
+				String mySentence = myList.get(x);
+				System.out.println ("mySentence: "+mySentence);
+				String myPOSString = posTagger.tagInternal(mySentence);
+				
+				System.out.println ("mySentence: "+ myPOSString);
+				
+				
+				// System.out.println(posTagger.tagSentence(mySentence.toString()) + " \r\n");
+				// String x = posTagger.tagSentence(mySentence.toString());
 			}
 		}
 		catch (IOException e) {
 			System.out.println("Error: "+e);
+		}catch (Exception e){
+			e.printStackTrace ();
 		}
 	}
 }
