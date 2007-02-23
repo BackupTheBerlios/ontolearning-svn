dc41cf3acbf55057955bcf57d9750372efb182f1
jasper <jasper@b5e4521c-5f29-0410-b9a4-cd8ebb1e93c5>
2007-02-23 09:41:16 +0000

    het probleem zat niet in de eerste if maar in de else, elke keer als er al data gevonden werd, werd deze door else gereset naar een lege string, door in de else een xtra id op te nemen wordt dit voorkomen en hoeft de file niet elke keer opnieuw doorzocht te worden voor abstracts.

--- OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/datatypes/impl/DocumentFile.java
+++ OntoLearn/src/nl/eur/eco_ict/seminar/ontolearn/datatypes/impl/DocumentFile.java
@@ -19,12 +19,13 @@ import nl.eur.eco_ict.seminar.ontolearn.datatypes.Document;
  */
 public class DocumentFile implements Document {
 	private File file = null;
+
 	private StringBuffer abstrct = null;
 
 	public DocumentFile (File f) {
 		this.file = f;
 	}
-	
+
 	/**
 	 * @return read the whole document/file
 	 * @throws FileNotFoundException
@@ -35,75 +36,79 @@ public class DocumentFile implements Document {
 
 	/**
 	 * @return the abstracts contained in this document (file)
-	 * @throws FileNotFoundException
+	 * @throws IOException
 	 */
-	public BufferedReader readAbstracts () throws FileNotFoundException {
-		// if (this.abstrct == null && this.isRedifDocument ()){
-		// Nico edit: door if abstrct == null zit de StringReader die gereturned word bij de 2e extractor op de EOF en krijgt deze extractor geen data.
-		if (this.isRedifDocument ()){
+	public BufferedReader readAbstracts () throws IOException {
+		if (this.abstrct == null && this.isRedifDocument ()) {
 			this.extractAbstracts ();
-		}else{
-			this.abstrct = new StringBuffer();
+		} else {
+			if (this.abstrct == null) {
+				this.abstrct = new StringBuffer ();
+			}
 		}
-		return new BufferedReader (new StringReader (this.abstrct.toString ()
-				));
+		return new BufferedReader (new StringReader (this.abstrct.toString ()));
 	}
-	
+
 	/**
 	 * @return the name of the document
 	 */
-	public String getName (){
+	public String getName () {
 		return this.file.getName ();
 	}
-	
+
 	/**
-	 * @return it's a redif document when it starts with 'Template-Type: ReDIF-Paper'
-	 * @throws FileNotFoundException 
+	 * @return it's a redif document when it starts with 'Template-Type:
+	 *         ReDIF-Paper'
+	 * @throws FileNotFoundException
 	 */
-	protected boolean isRedifDocument () throws FileNotFoundException{
+	protected boolean isRedifDocument () throws FileNotFoundException {
 		final String header = "Template-Type: ReDIF-Paper";
-		if (this.file != null && this.file.canRead ()){
-			BufferedReader br = new BufferedReader (new FileReader(this.file));
+		if (this.file != null && this.file.canRead ()) {
+			BufferedReader br = new BufferedReader (new FileReader (this.file));
 			String line = null;
 			try {
-				if ((line=br.readLine ())!=null){
+				if ((line = br.readLine ()) != null) {
 					return line.contains (header);
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				e.printStackTrace ();
 			}
 		}
 		return false;
 	}
-	
+
 	/**
-	 * Assumes it's a REDIF file and searches the file for abstracts. When an abstract is found it is put into the global StringBuffer (a cache for repeated requests)
+	 * Assumes it's a REDIF file and searches the file for abstracts. When an
+	 * abstract is found it is put into the global StringBuffer (a cache for
+	 * repeated requests)
 	 * @throws FileNotFoundException
 	 */
-	protected void extractAbstracts () throws FileNotFoundException{
+	protected void extractAbstracts () throws FileNotFoundException {
 		final String abstractHeader = "Abstract:";
 		final String regEndAbstract = "^([a-zA-Z\\x2D]+)(\\x3A{1})( )(.)*$";
-		this.abstrct = new StringBuffer();
-		BufferedReader br = new BufferedReader(new FileReader(this.file));
+		this.abstrct = new StringBuffer ();
+		BufferedReader br = new BufferedReader (new FileReader (this.file));
 		String line = null;
 		boolean extract = false;
-		
-		try{
-		while((line = br.readLine ()) != null){
-			if (extract && line.matches (regEndAbstract)){
-				extract = false;
-			}
-			if (extract){
-				this.abstrct.append (line.trim()+" ");
-			}
-			if (!extract && line.startsWith (abstractHeader)){
-				extract = true; // take the next lines
-				if(line.length() > 9) {
-					this.abstrct.append (line.substring (10, line.length ()).trim()+" ");
+
+		try {
+			while ((line = br.readLine ()) != null) {
+				if (extract && line.matches (regEndAbstract)) {
+					extract = false;
 				}
-			}			
-		}
-		}catch(Exception e){
+				if (extract) {
+					this.abstrct.append (line.trim () + " ");
+				}
+				if (!extract && line.startsWith (abstractHeader)) {
+					extract = true; // take the next lines
+					if (line.length () > 9) {
+						this.abstrct.append (line
+								.substring (10, line.length ()).trim ()
+								+ " ");
+					}
+				}
+			}
+		} catch (Exception e) {
 			e.printStackTrace ();
 		}
 	}
